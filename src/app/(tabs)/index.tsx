d6478aecbb9cf07972081ac5007c0a81fb78948e
lucidjug <tuuju2@gmail.com>
2025-03-07 15:38:00 +0700

    video 59

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+
+import { Text, View } from "react-native"
+
+const HomeTab = () => {
+
+
+    return (
+        <View>
+            <Text>welcome to HomeTab</Text>
+        </View>
+    )
+}
+
+export default HomeTab
---
+++ src/app/(tabs)/setting.tsx
@@ -0,0 +1,14 @@
+
+import { Text, View } from "react-native"
+
+const SettingTab = () => {
+
+
+    return (
+        <View>
+            <Text>welcome to Setting Page</Text>
+        </View>
+    )
+}
+
+export default SettingTab
