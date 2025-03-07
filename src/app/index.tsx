1e153033f7d8b0f1b3e59a1480efd4600bf30e6f
lucidjug <tuuju2@gmail.com>
2025-03-07 13:50:00 +0700

    video 55

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native"
+
+const AppRoot = () => {
+    return (
+        <View>
+            <Text>
+                1 Hello World with react Native
+            </Text>
+        </View>
+    )
+}
+
+export default AppRoot
