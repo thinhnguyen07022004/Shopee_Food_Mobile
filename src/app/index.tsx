06bfd2820e99652aab1fd23008f2cf02fe454c60
lucidjug <tuuju2@gmail.com>
2025-03-13 16:01:31 +0700

    video 75

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,28 +1,28 @@
-import { StyleSheet, Text, View } from "react-native"
+import { Image, ImageBackground, StyleSheet, Text, View } from "react-native"
 import ShareButton from "components/button/share.button"
 import { APP_COLOR } from "utils/constant"
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import bg from '@/assets/auth/welcome-background.png'
+import falogo from '@/assets/auth/facebook.png'
+import gglogo from '@/assets/auth/google.png'
+import { LinearGradient } from 'expo-linear-gradient';
+import TextBetweenLine from "@/components/button/text.between.line";
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        borderWidth: 5,
-        borderColor: "red"
+        marginHorizontal: 10,
 
     },
     welcomeText: {
         flex: 0.6,
-        borderWidth: 5,
-        borderColor: "green",
         alignItems: "flex-start",
         justifyContent: "center",
         paddingLeft: 20,
     },
     welcomeButton: {
         flex: 0.4,
-        borderWidth: 5,
-        borderColor: "grey",
-        gap: 20
+        gap: 30
     },
     header: {
         fontSize: 40,
@@ -40,78 +40,102 @@ const styles = StyleSheet.create({
 })
 const WelcomePage = () => {
     return (
-        <View style={styles.container}>
-            <View style={styles.welcomeText}>
-                <Text style={styles.header}>
-                    Welcome to
-                </Text>
-                <Text style={styles.body}>
-                    @HoidanIT - Food
-                </Text>
-                <Text style={styles.footer}>
-                    Nền tảng giao đồ ăn trực tuyến hàng đầu Việt Nam
-                </Text>
-            </View>
-            <View style={styles.welcomeButton}>
-                <Text>
-                    Đăng nhập với
-                </Text>
-                <View style={{
-                    flexDirection: "row",
-                    justifyContent: "center",
-                    gap: 30,
-                }}>
-                    <ShareButton
-                        title="Facebook"
-                        onPress={() => alert("me")}
-                        textStyle={{ textTransform: "uppercase" }}
-                        buttonStyle={{
+        <ImageBackground
+            style={{ flex: 1 }}
+            source={bg}
+        >
+            <LinearGradient
+                style={{ flex: 1 }}
+                colors={['transparent', '#191B2F']}
+                locations={[0.2, 0.8]}
+            >
+                <View style={styles.container}>
+                    <View style={styles.welcomeText}>
+                        <Text style={styles.header}>
+                            Welcome to
+                        </Text>
+                        <Text style={styles.body}>
+                            @HoidanIT - Food
+                        </Text>
+                        <Text style={styles.footer}>
+                            Nền tảng giao đồ ăn trực tuyến hàng đầu Việt Nam
+                        </Text>
+                    </View>
+                    <View style={styles.welcomeButton}>
+                        <TextBetweenLine title="Đăng nhập với" />
+                        <View style={{
+                            flexDirection: "row",
                             justifyContent: "center",
-                            borderRadius: 30,
-                            backgroundColor: "#fff"
-                        }}
-                        icon={
-                            <FontAwesome5 name="facebook" size={24} color="black" />
-                        }
-                    />
-                    <ShareButton
-                        title="Google"
-                        onPress={() => alert("me")}
-                        textStyle={{ textTransform: "uppercase" }}
-                        buttonStyle={{
-                            justifyContent: "center",
-                            borderRadius: 30,
-                            paddingHorizontal: 20,
-                            backgroundColor: "#fff"
-                        }}
-                        icon={
-                            <FontAwesome5 name="google" size={24} color="black" />
-                        }
-                    />
+                            gap: 30,
+                        }}>
+                            <ShareButton
+                                title="Facebook"
+                                onPress={() => alert("me")}
+                                textStyle={{ textTransform: "uppercase" }}
+                                buttonStyle={{
+                                    justifyContent: "center",
+                                    borderRadius: 30,
+                                    backgroundColor: "#fff"
+                                }}
+                                icon={
+                                    <Image source={falogo} />
+                                }
+                            />
+                            <ShareButton
+                                title="Google"
+                                onPress={() => alert("me")}
+                                textStyle={{ textTransform: "uppercase" }}
+                                buttonStyle={{
+                                    justifyContent: "center",
+                                    borderRadius: 30,
+                                    paddingHorizontal: 20,
+                                    backgroundColor: "#fff"
+                                }}
+                                icon={
+                                    <Image source={gglogo} />
+                                }
+                            />
+                        </View>
+                        <View>
+                            <ShareButton
+                                title="Đăng nhập với email"
+                                onPress={() => alert("me")}
+                                textStyle={{ color: "#fff", paddingVertical: 5 }}
+                                buttonStyle={{
+                                    justifyContent: "center",
+                                    borderRadius: 30,
+                                    marginHorizontal: 50,
+                                    paddingHorizontal: 10,
+                                    backgroundColor: "#2c2c2c",
+                                    borderColor: "#505050",
+                                    borderWidth: 1
+                                }}
+                                pressStyle={{ alignSelf: "stretch" }}
+                            />
+                        </View>
+                        <View style={{
+                            flexDirection: "row",
+                            gap: 10,
+                            justifyContent: "center"
+                        }}>
+                            <Text style={{
+                                textAlign: "center",
+                                color: "white"
+                            }}>
+                                Chưa có tài khoản?
+                            </Text>
+                            <Text style={{
+                                textAlign: "center",
+                                color: "white",
+                                textDecorationLine: "underline"
+                            }}>
+                                Đăng ký.
+                            </Text>
+                        </View>
+                    </View>
                 </View>
-                <View>
-                    <ShareButton
-                        title="Đăng nhập với email"
-                        onPress={() => alert("me")}
-                        textStyle={{ color: "#fff", paddingVertical: 5 }}
-                        buttonStyle={{
-                            justifyContent: "center",
-                            borderRadius: 30,
-                            marginHorizontal: 50,
-                            paddingHorizontal: 10,
-                            backgroundColor: "#2c2c2c"
-                        }}
-                        pressStyle={{ alignSelf: "stretch" }}
-                    />
-                </View>
-                <View>
-                    <Text style={{ textAlign: "center" }}>
-                        Chưa có tài khoản? Đăng ký.
-                    </Text>
-                </View>
-
-            </View>
-        </View>
+            </LinearGradient>
+        </ImageBackground>
 
     )
 }
