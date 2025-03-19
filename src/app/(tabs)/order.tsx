f9fb281846382e68b25849c38b2153d8fea7aac5
lucidjug <tuuju2@gmail.com>
2025-03-19 13:18:31 +0700

    video 110

---
+++ src/app/(tabs)/order.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+const OrderPage = () => {
+    return (
+        <View>
+            <Text>Order Page</Text>
+        </View>
+    )
+}
+
+export default OrderPage
